56548a1f6617a580e7e433f617db1964eb69541a
wh2887 <13567272790@163.com>
2020-06-13 13:11:25 +0800

    init

---
+++ McCall-com/public/McCall.js
@@ -0,0 +1,24 @@
+//封装
+function jsonp(url) {
+  return new Promise((resolve, reject) => {
+    const random = Math.random();
+    console.log(random);
+    window[random] = (data) => {
+      resolve(data);
+    };
+
+    const script = document.createElement("script");
+    script.src = `${url}?callback=${random}`;
+    script.onload = () => {
+      script.remove();
+    };
+    script.onerror = () => {
+      reject();
+    };
+    document.body.appendChild(script);
+  });
+}
+//使用
+jsonp("http://qq.com:8888/friends.js").then((data) => {
+  console.log(data);
+});
---
+++ README.md
@@ -0,0 +1,6 @@
+### 跨域的学习
+
+- CORS
+- JSONP
+
+[博客链接](https://www.yuque.com/docs/share/15162e29-a2bf-4451-b305-df10a79ed500?#)
